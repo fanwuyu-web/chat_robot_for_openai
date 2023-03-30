ca070d733f205fc2547d43739a528424b1cc26e6
qkr141 <85012645+qkr141@users.noreply.github.com>
2023-03-30 20:03:42 +0800

    Update gptApi.py

--- gptApi.py
+++ gptApi.py
@@ -10,7 +10,7 @@ warnings.filterwarnings("ignore")
 #openai没办法使用全局代理，在lib\site-packages\openai\api_requestor.py文件里添加代理即可
 
 # ssl._create_default_https_context = ssl._create_unverified_context
-# openai.api_key = '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'
+# openai.api_key = '*********'
 # # openai.proxy = 'http://202.79.168.46:6666'
 # # openai.verify_ssl_certs = False
 # openai.api_base='https://202.79.168.46/v1'
@@ -27,7 +27,7 @@ warnings.filterwarnings("ignore")
 
 class GptApi:
     def __init__(self):
-        self.api_key='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'
+        self.api_key='***********'
         openai.api_key = self.api_key
         openai.api_base='https://202.79.168.46/v1'
 
